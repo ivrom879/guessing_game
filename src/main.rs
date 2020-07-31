729b8857f7b50c6cd9bb444728a6c6f94eba53e8
ivrom879 <ivrom879@yandex.ru>
2020-08-01 00:03:01 +0300

    fix

--- src/main.rs
+++ src/main.rs
@@ -16,7 +16,7 @@ fn main() {
         io::stdin().read_line(&mut guess)
             .expect("Failed to read line");
 
-        let guess: u32 = guess.trim().parse() {
+        let guess: u32 = match guess.trim().parse() {
             Ok(num) => num,
             Err(_) => continue,
         };
@@ -25,7 +25,7 @@ fn main() {
 
         match guess.cmp(&secret_number) {
             Ordering::Less => println!("Too small!"),
-            Ordering::Greater => println!("Too big!");
+            Ordering::Greater => println!("Too big!"),
             Ordering::Equal => {
                 println!("You Win!");
                 break;
